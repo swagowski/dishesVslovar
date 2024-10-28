c003f1acb17f6fb85f26cdab06ca73c83c29fccd
swagowski <nikitin.ss@inbox.ru>
2024-10-28 23:49:16 +0300

    Доработка для решения условий задачи №2

--- main.py
+++ main.py
@@ -18,12 +18,31 @@ def read_recipes(files_path):
             recipes[dish_name] = ingredients
     return recipes
 
+def get_shop_list_by_dishes(dishes, person_count,recipes):
+    """Возвращает список ингредиентов для заданных блюд и количества персон."""
+    shop_list = {}
+
+    for dish in dishes:
+        if dish in recipes:
+            for ingredient in recipes[dish]:
+                ingredient_name = ingredient['ingredient_name']
+                measure = ingredient['unit']
+                quantity = ingredient['quantity'] * person_count
+
+                if ingredient_name in shop_list:
+                    shop_list[ingredient_name]['quantity'] += quantity
+                else:
+                    shop_list[ingredient_name] = {'unit': measure, 'quantity': quantity}
+
+    return shop_list
+
 
 def main():
     file_path = 'C:\\Users\\semen\\PycharmProjects\\pythonProject\\dishes.txt'
     recipes = read_recipes(file_path)
     print(recipes)
 
-
+    result = get_shop_list_by_dishes(['Утка по-пекински', 'Омлет'], 3, recipes)
+    print(result)
 if __name__ == "__main__":
     main()
